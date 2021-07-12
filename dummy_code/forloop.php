380b3593bc0f9e9798c2ce55e7b1ebd443d094ad
Anthony Perez <anthony.perez@Simpaticos-iMac.cev.cevmultimedia.com>
2021-07-12 16:12:29 -0500

    forloop and while loop added

---
+++ dummy_code/forloop.php
@@ -0,0 +1,7 @@
+<?php
+    $food = array("turkey", "milk", "apples");
+
+    for ($i=0; $i < count($food); $i++) { 
+        echo $food[$i] . "\n";
+    }
+?>
--- dummy_code/multidimensional.php
+++ dummy_code/multidimensional.php
@@ -4,6 +4,19 @@
             "name" => "Anthony",
             "age" => 27,
             "location" => "Lubbock",
-        )
-    );
+            "education_level" => "College"
+            ),
+            "Hobbies" => array(
+                "Video Games",
+                "Streaming",
+                "Music"
+            )
+            );
+
+            echo "My name is " . $user["info"]["name"] . ".\n";
+            echo "I am " . $user["info"]["age"] . " years old. \n";
+            echo "I live in " . $user["info"]['location']. ".\n";
+            echo "My latest education level is " . $user['info']['education_level']. ".\n";
+
+            echo "I enjoy " . $user["Hobbies"][0] . ", " . $user["Hobbies"][1] . ", " . $user["Hobbies"][2].".\n";
 ?>
