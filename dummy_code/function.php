62ee2d0477326cd45da221b29bf9087bee936ee6
Anthony Perez <anthony.perez@Simpaticos-iMac.cev.cevmultimedia.com>
2021-07-14 16:46:50 -0500

    finishing chapters 2 & 3

---
+++ dummy_code/function.php
@@ -0,0 +1,11 @@
+<?php
+    $sweaterPrice = 50;
+    $percentOff = 0.25;
+
+    function couponCode($price, $discount){
+        return $price * $discount;
+    }
+
+    echo "The sweater originally costs $" . $sweaterPrice . " with the discount you'll pay $" 
+    . ($sweaterPrice - couponCode($sweaterPrice, $percentOff)) . "\n";
+?>
